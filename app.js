1199b97f589b0bda6ba4b09b553647c9fe8a9e13
yamasya54 <yavuzamasya1@gmail.com>
2023-12-28 12:02:19 +0300

    v2

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express =require('express')
+const app = express();
 
-const app = express()
+//Template Engine
+app.set("view engine", "ejs");
 
-app.get('/' , (req,res) => {
-res.send('Başlıyoruz')
-} )
+//Middlewares
+app.use(express.static("public"));
 
+//Routes
+app.get('/', (req, res) => {
 
+  res.status(200).render('index', {
+      page_name: "index"
+  });
+});
 
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
+  });
 
-
-
-
-
-const port = 3000
-app.listen(port, () =>{
-    console.log(`App started on port ${port}`);
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`)
