666a690da09c11e9f8627c5e012fbe39c4b2c5df
yamasya54 <yavuzamasya1@gmail.com>
2023-12-28 15:48:35 +0300

    8.ders

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 const app = express();
 
+//Connect DB
+mongoose
+  .connect('mongodb://localhost/smartedu-db')
+  .then(() => {
+    console.log('DB Connected Successfully');
+  });
+
+
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Middlewares
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Routes
-app.get('/', (req, res) => {
-  
-  res.status(200).render('index', {
-      page_name: "index"
-  });
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
-    });
-  });
-
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
